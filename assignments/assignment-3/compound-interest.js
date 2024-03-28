e3deca647a56a0acea6aa3b62be7ce2f4a6a5d90
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-03-28 16:58:43 -0400

    Push not saved files

---
+++ assignments/assignment-3/compound-interest.js
@@ -0,0 +1,7 @@
+var p = 60;
+var r = 40;
+var n = 10;
+var t = 30;
+
+var principal = (p * (1 + r / n)) ^ (n * t);
+console.log(principal);
--- assignments/assignment-3/temperature.js
+++ assignments/assignment-3/temperature.js
@@ -1,3 +1,9 @@
-var Fahrenheit = 70;
+var F = 70;
 
-console.log((celsius = (Fahrenheit - 32) * (5 / 9)));
+console.log((C = (F - 32) * (5 / 9)));
+console.log(C); // 21.11111111111111
+
+var C = 100;
+
+console.log((F = C * (9/5) + 32));
+console.log(F);
