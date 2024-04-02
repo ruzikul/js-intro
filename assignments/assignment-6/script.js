0ed64cc2ae49334ed11275b4a925db95fd57114a
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-02 13:49:56 -0400

    Finish assignment-6

---
+++ assignments/assignment-6/script.js
@@ -0,0 +1,16 @@
+var name = "Aziz"
+var age = "16"
+var cityOfResidence = "New York"
+var profession = "Student"
+var school = "James Madison High School"
+var hobby = "Gym"
+var favoriteFood = "Plov"
+var favoriteColor = "Black"
+var favoriteMovie = "Spiderman: No way home"
+var favoriteSport = "Soccer"
+var dreamTravelDestination = "Paris"
+
+//---------------------------------------------//
+
+var aboutMe = `Hi, My name is ${name} and I am ${age} years old. Currently I live in ${cityOfResidence} and I am a ${profession} at ${school}. My favorite hobby is to go to the ${hobby} and my favorite food is ${favoriteFood}. My favorite color is ${favoriteColor} and I really like the movie "${favoriteMovie}". I also like to play ${favoriteSport}. My dream is to one day visit ${dreamTravelDestination}.`
+console.log(aboutMe)
