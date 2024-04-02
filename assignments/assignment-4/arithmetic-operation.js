dab1d7b66c7455d879665671c92157aed86b5491
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-02 12:29:28 -0400

    Finish assignment-4

---
+++ assignments/assignment-4/arithmetic-operation.js
@@ -0,0 +1,7 @@
+var num1 = 10;
+var num2 = 15;
+
+console.log('Addition:', num1 + num2) // 25
+console.log('Subtraction:', num1 - num2) // -5
+console.log('Multiplacation:', num1 * num2) // 150
+console.log('Division:', num1 / num2) // 0.6666666666666666
