bea4604ec0730c4bb2e68e66736e18c611e038c2
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-04 17:53:44 -0400

    In progress assessment-3

---
+++ assessments/assessment-3/solution.js
@@ -0,0 +1,46 @@
+//---------------------------------------------\\
+        // Challenge 1
+
+var challenge1 = "I Love JavaScript";
+var new1 = (challenge1.replaceAll(" ", ''))
+console.log(new1);
+
+
+//---------------------------------------------\\
+        // Challenge 2
+
+var javascript = "javascript";
+var challenge2 = (javascript.replace("javascript", 'JavaScript'))
+console.log(challenge2);
+
+
+//---------------------------------------------\\
+        // Challenge 3
+
+var fullName = "John Doe"
+var initial = "JD"
+console.log(fullName);
+console.log(initial);
+
+
+//---------------------------------------------\\
+        // Challenge 4
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var alphabetWithoutMyNameFirstCharacter = alphabet.replace("a", "")
+console.log(alphabetWithoutMyNameFirstCharacter);
+
+
+//---------------------------------------------\\
+        // Challenge 5
+
+var completeSentence = "The quick brown fox jumps over the lazy dog."
+var randomChar = "u"
+
+
+//---------------------------------------------\\
+        // Challenge 6
+
+var randomString = "Greetings";
+var randomCharacter = "e";
+var new2 = (randomString.length)
