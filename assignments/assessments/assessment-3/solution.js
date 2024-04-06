29247f225324a50a43a31485719762fe9974ae05
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-06 14:21:08 -0400

    Created solution.js

---
+++ assignments/assessments/assessment-3/solution.js
@@ -0,0 +1,46 @@
+//---------------------------------------------\\
+        // Challenge 1
+
+        var challenge1 = "I Love JavaScript";
+        var new1 = (challenge1.replaceAll(" ", ''))
+        console.log(new1);
+        
+        
+        //---------------------------------------------\\
+                // Challenge 2
+        
+        var javascript = "javascript";
+        var challenge2 = (javascript.replace("javascript", 'JavaScript'))
+        console.log(challenge2);
+        
+        
+        //---------------------------------------------\\
+                // Challenge 3
+        var fullName = "John Doe";
+        var initial = fullName[0];
+        var initial2 = fullName[5];
+        var result = initial.concat(initial2);
+        console.log(result);
+        
+        
+        //---------------------------------------------\\
+                // Challenge 4
+        
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        var alphabetWithoutMyNameFirstCharacter = alphabet.replace("a", "")
+        console.log(alphabetWithoutMyNameFirstCharacter);
+        
+        
+        //---------------------------------------------\\
+                // Challenge 5
+        
+        var completeSentence = "The quick brown fox jumps over the lazy dog."
+        var randomChar = "u"
+        
+        
+        //---------------------------------------------\\
+                // Challenge 6
+        
+        var randomString = "Greetings";
+        var randomCharacter = "e";
+        var new2 = (randomString.length)
