e9d529327b62f5c3eaf8deac583c38465aa3981c
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-13 16:02:50 -0400

    Finish assignment-12

---
+++ assignments/assignment-12/script.js
@@ -0,0 +1,27 @@
+// --- Generate a Random Username --- \\
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+if (random > 1 && random < 3) {
+    console.log("Bob");
+} else if (random > 4 && random < 7) {
+    console.log("Martin");
+} else {
+    console.log("Jennifer");
+}
+
+
+// --- Greeting --- \\
+
+var randomNumber = Math.floor(Math.random() * 4);
+var name = "Aziz";
+
+if (randomNumber === 1) {
+    console.log(`Dear ${name}, wishing you a day filled with happiness and a year filled with joy.`)
+} else if (randomNumber === 2) {
+    console.log(`Dear ${name}, sending you smiles for every moment of your special day.`)
+} else if (randomNumber === 3) {
+    console.log(`Dear ${name}, have a wonderful time and a very happy day!`)
+} else {
+    console.log(`Dear ${name}, hope your special day brings you all that your heart desires!`)
+}
