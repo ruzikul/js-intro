edabf21a9bc9d728f041a20ef93718fee452a534
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-15 21:24:01 -0400

    Start assignment-13

---
+++ assignments/assignment-13/script.js
@@ -0,0 +1,29 @@
+// --- Time gen --
+
+var timeGen = Math.floor(Math.random() * 86400);
+
+// --- Time conversion --
+
+
+
+
+
+
+
+
+
+// --- Greeting Message Based on Time --
+
+var randomTime = Math.floor(Math.random() * 24);
+
+if (randomTime = 0 && randomTime <= 12) {
+    console.log("Good Morning!")
+} else if (randomTime >= 12 && randomTime <= 17) {
+    console.log("Good Afternoon!");
+} else {
+    console.log("Good Evening!")
+}
+
+// --- Greeting Message Based on Time --
+
+
