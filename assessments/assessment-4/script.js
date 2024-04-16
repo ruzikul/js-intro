b6ffcfe1c16513f3710c190e3eb97807a0e3e732
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-15 21:08:15 -0400

    Start assessment-4

---
+++ assessments/assessment-4/script.js
@@ -0,0 +1,34 @@
+// --- Challenge 1 --- \\
+
+var singleWord = "Greeting";
+var index = singleWord.length - 1;
+var random1 = Math.floor(Math.random() * 8);
+var random2 = Math.floor(Math.random() * 8);
+
+var char1 = singleWord[random1];
+var char2 = singleWord[random2];
+console.log(char1.toUpperCase(), char2.toUpperCase())
+
+// --- Challenge 2 --- \\
+
+var randomNumber = Math.floor(Math.random() * 1000);
+
+var number1 = randomNumber % 2 === 0;
+console.log(number1);
+
+var
+
+
+// --- Challenge 3 --- \\
+
+
+
+
+// --- Challenge 4 --- \\
+
+
+
+// --- Challenge 5 --- \\
+
+
+// --- Challenge 6 --- \\
