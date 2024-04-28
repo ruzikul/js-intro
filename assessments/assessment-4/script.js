d8993a61a3e1d941f8b807f5b3cce6386585711d
Aziz Ruzikulov <ruzikulov.dev@gmail.com>
2024-04-28 17:31:54 -0400

    Finish assessment-4

--- assessments/assessment-4/script.js
+++ assessments/assessment-4/script.js
@@ -13,22 +13,33 @@ console.log(char1.toUpperCase(), char2.toUpperCase())
 
 var randomNumber = Math.floor(Math.random() * 1000);
 
-var number1 = randomNumber % 2 === 0;
-console.log(number1);
-
-var
-
+console.log(randomNumber % 2 !== 0);
+console.log(randomNumber % 2 === 0);
 
 // --- Challenge 3 --- \\
 
-
-
+var numerator = Math.floor(Math.random() * (-100 + 1) - 1);
+var denominator = Math.floor(Math.random() * (8 - 5 + 1) + 5);
+var result = numerator / denominator;
+console.log('Original result: ' + result);
+console.log('Rounded result: ' + Math.round(result));
 
 // --- Challenge 4 --- \\
 
-
+var testNumber = Math.floor(Math.random() * (2500 - 25 + 1) + 25);
+console.log(testNumber % 3 === 0 && testNumber % 5 !== 0);
 
 // --- Challenge 5 --- \\
 
+var divTest = Math.floor(Math.random() * (200 - 100 + 1) + 100);
+
+console.log(divTest % 5 === 0 && divTest % 3 !== 0)
 
 // --- Challenge 6 --- \\
+
+var creativeString = "Exploring the vast universe of code can lead to unexpected discoveries."
+
+var index = Math.floor(Math.random() * creativeString.length);
+var extractedText = creativeString.slice(randomIndex + 1);
+
+console.log('Text after index ' + index + ': ' + extractedText);
